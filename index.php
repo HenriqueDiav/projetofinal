1f5f272eaff8cc530cade83652e0138d4ced2578
HenriqueDiav <henriquetostadias@gmail.com>
2023-02-08 14:44:22 -0300

    ultima versao

--- index.php
+++ index.php
@@ -154,6 +154,8 @@ if (isset($_POST['salvar'])) {
 
 </section> -->
 
+
+
 <?php
 require('./conexao.php');
 
@@ -198,3 +200,26 @@ if (isset($_POST['salvar'])) {
     </div>
 
 </section>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Twitter <i class="fa-brands fa-twitter"></i> <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Instagram <i class="fa-brands fa-instagram"></i></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
